e2640767c28ae704d3951f5bbd3fc17acc0a0e83
seong in Hong <75526895+espada105@users.noreply.github.com>
2023-10-26 01:33:55 +0900

    1
    1

--- POWER_JAVA/CH32.java
+++ POWER_JAVA/CH32.java
@@ -1,5 +1,17 @@
 package POWER_JAVA;
 
 public class CH32 {
-    
-}
+    public static void main(String[] args){
+        for(int i = 1; i <51; i++){
+
+            if((i%10 == 3)||(i%10 == 6)||(i%10 == 9)){
+                if((i/10 == 3)||(i/10 == 6)||(i/10 == 9)){
+                    System.out.println("짝짝");
+                }else{
+                    System.out.println("짝");
+                }
+            }
+            System.out.println(i);
+            }
+        }
+    }
--- POWER_JAVA/midterm1.java
+++ POWER_JAVA/midterm1.java
@@ -1,16 +1,30 @@
 package POWER_JAVA;
 
-public class midterm1 {
-    public static void main(String [] args){
-    
-    for(int i = 0; i < 10; i++){
-        System.out.println(i + "");
-        System.out.print(i);
+class midterm12 {
+    String color;
+    int speed;
+    int gear;
+
+    @Override
+    public String toString(){
+        return "색"+color+"speed"+speed+"gear"+gear;
     }
-    
+    void changeGear(int g) {gear=g;}
+    void speedUp(){speed=speed+10;}
+    void speedDown(){speed = speed-10;}
+
+
 
     }
-}
+    public class midterm1{
+        public static void main(String[]args){
+
+            midterm12 myCar = new midterm12();
+            myCar.changeGear(1);
+            myCar.speedUp();
+            System.out.println(myCar);
+        }
+    }
 
 
 
