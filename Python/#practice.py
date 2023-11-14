4893b482af26a9bf7435946876111919d59aa099
seong in Hong <75526895+espada105@users.noreply.github.com>
2023-11-14 13:37:36 +0900

    1
    1

--- Python/#practice.py
+++ Python/#practice.py
@@ -1,15 +1,111 @@
-def mul(choice,*x):
-    if choice=="add":
-        result = 0
-        for i in x:
-            result+= i
-    elif choice =="div":
-        result = 0
-        for i in x:
-            result/= i
-    elif choice =="mul":
-        result = 0
-        for i in x:
-            result*= i
-    print(result)
-mul("add",1,2,3)
+# f = open("/Users/hongseong-in/Documents/GitHub/Major_Study/Python/#새파일.txt",'w')
+
+# for i in range(1,11):
+#     data = "%d 번쨰 줄입니다"%i
+#     f.write(data)
+#     print(data)
+# f.close()
+
+
+
+# result = 0
+# result2 = 0
+# def add(num):
+#     global result
+#     result += num
+#     return result
+
+# print(add(3))
+# print(add(4))
+
+
+
+# def minus(num):
+#     global result2
+#     result2 -= num
+#     return result2
+
+# print(minus(3))
+# print(minus(4))
+
+# def mul(num:)
+#     global result3
+#     result3*
+
+#시험공부
+# class lastterm()
+#     def plan():
+#         sub = input()
+
+#     def study():
+
+
+
+
+
+# #은행계좌
+# 계좌번호
+# 아이디 비밀번호
+
+
+
+
+class Cal:
+    def __init__(self):
+        self.result = 0
+
+    def add (self , num):
+        self.result +=num
+        return self.result
+    
+    def sub (self , num):
+        self.result -=num
+        return self.result
+    
+    def mul (self , num):
+        self.result *=num
+        return self.result
+    
+    def div (self , num):
+        self.result /=num
+        return self.result
+
+# cal1 = Cal()
+
+# cal1.add(10)
+
+
+
+
+
+
+class AdvancedCal(Cal):
+    def mod(self, num):
+        self.result %= num
+        return self.result
+    
+    def quo(self, num):
+        if num != 0:
+            self.result //= num
+            return self.result
+        else:
+            print("나누는 수는 0이 아니어야 합니다.")
+            return None
+# 테스트
+adv_cal = AdvancedCal()
+
+# 더하기
+adv_cal.add(10)
+print("덧셈 결과:", adv_cal.result)
+
+# 빼기
+adv_cal.sub(5)
+print("뺄셈 결과:", adv_cal.result)
+
+# 나머지
+adv_cal.mod(3)
+print("나머지 결과:", adv_cal.result)
+
+# 몫
+adv_cal.quo(2)
+print("몫 결과:", adv_cal.result)
