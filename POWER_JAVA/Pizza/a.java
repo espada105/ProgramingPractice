e8457c267b33832749fbbb0df6d10f1c36731448
seong in Hong <75526895+espada105@users.noreply.github.com>
2023-12-08 16:50:19 +0900

    /

--- POWER_JAVA/Pizza/a.java
+++ POWER_JAVA/Pizza/a.java
@@ -1,23 +1,23 @@
-package POWER_JAVA.Pizza;
+// package POWER_JAVA.Pizza;
 
-class A {
-    public void print() {
-        System.out.println("A print");
-    }
-}
+// class A {
+//     public void print() {
+//         System.out.println("A print");
+//     }
+// }
 
-class B {
-    public void print() {
-        System.out.println("B print");
-    }
-}
+// class B {
+//     public void print() {
+//         System.out.println("B print");
+//     }
+// }
 
-public class a {
-    public static void main(String[] args) {
-        B objB = new B();
-        objB.print();
+// public class a {
+//     public static void main(String[] args) {
+//         B objB = new B();
+//         objB.print();
 
-//        A objA = new B();
-//        objA.print();
-    }
-}
+// //        A objA = new B();
+// //        objA.print();
+//     }
+// }
--- POWER_JAVA/PizzaTest.java
+++ POWER_JAVA/PizzaTest.java
@@ -1,26 +1,26 @@
-package POWER_JAVA;
+// package POWER_JAVA;
 
-class Pizza {
-    int radius;
+// class Pizza {
+//     int radius;
 
-    Pizza(int r) {
-        radius = r;
-    }
+//     Pizza(int r) {
+//         radius = r;
+//     }
 
-    static Pizza whosLargest(Pizza p1, Pizza p2) {
-        if (p1.radius > p2.radius)
-            return p1;
-        else
-            return p2;
-    }
-}
+//     static Pizza whosLargest(Pizza p1, Pizza p2) {
+//         if (p1.radius > p2.radius)
+//             return p1;
+//         else
+//             return p2;
+//     }
+// }
 
-public class PizzaTest {
-    public static void main(String args[]) {
-        Pizza obj1 = new Pizza(14);
-        Pizza obj2 = new Pizza(18);
+// public class PizzaTest {
+//     public static void main(String args[]) {
+//         Pizza obj1 = new Pizza(14);
+//         Pizza obj2 = new Pizza(18);
 
-        Pizza largest = Pizza.whosLargest(obj1, obj2);
-        System.out.println(largest.radius + "인치 피자가 더 큼.");
-    }
-}
+//         Pizza largest = Pizza.whosLargest(obj1, obj2);
+//         System.out.println(largest.radius + "인치 피자가 더 큼.");
+//     }
+// }
